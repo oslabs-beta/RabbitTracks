a9fcd7088fe128af6355b9afc5d64b75c916f47d
Jake Kazi <kazijake@gmail.com>
2022-08-19 07:33:55 +0600

    jk 8.19.22

--- server/server.js
+++ server/server.js
@@ -1,13 +1,14 @@
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
 const PORT = process.env.PORT;
 
-const app = express()
-  DIST_DIR = path.join(__dirname, '../build/')
-  HTML_FILE = path.join(DIST_DIR, 'index.html')
+const app = express();
+DIST_DIR = path.join(__dirname, "../build/");
+HTML_FILE = path.join(DIST_DIR, "index.html");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -15,22 +16,29 @@ app.use(cookieParser());
 
 // Serve static files:
 app.use(express.static(DIST_DIR));
-app.use(express.static('../src/assets'));
+app.use(express.static("../src/assets"));
 
 // Serve index.html
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.status(200).sendFile(path.resolve(__dirname, index.html));
 });
 
+//Passing a connection URI
+const sequelize = new Sequelize(
+  "postgres://rqbingxx:76BNWZGhlG4TBY8mgdFyHRtJ0hNEdhQU@suleiman.db.elephantsql.com/rqbingxx"
+);
+
+try {
+  sequelize.authenticate();
+  console.log("Connection has been established successfully.");
+} catch (error) {
+  console.error("Unable to connect to the database:", error);
+}
 
 // Routes
 
-
-
-
 // 404 Catch-All
 app.use("*", (req, res) => res.status(404).send("Not Found"));
-
 
 // Universal Error Handler
 app.use((err, req, res, next) => {
@@ -44,11 +52,9 @@ app.use((err, req, res, next) => {
   return res.status(errorObj.status).json(errorObj.message);
 });
 
-
 // Start Server
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
-
 
 module.exports = app;
