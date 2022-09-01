f51d629d55a373198854f1ad23e31ca90ad6fbf1
Glen <glen.kasoff@gmail.com>
2022-08-31 20:19:02 -0400

    consume update

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -1,1 +1,29 @@
-// get all messages ..... put in server.js/routes too!
+// post request (will be from consume message)
+
+// get all messages for loading page ..... put in server.js/routes too!
+const db = require("../models/elephantsql");
+
+const getMessages = (req, res, next) => {
+  // ***need to have project id from user here
+  const projID = 1;
+  const text = `SELECT * FROM messages WHERE project_id = ${projID}`;
+
+  db.query(text)
+    .then((data) => {
+      res.locals.messages = data.rows;
+      return next();
+    })
+    .catch((err) => {
+      return next({ log: err, message: { err: "catch in getMessages" } });
+    });
+};
+
+const addMessage = (req, res, next) => {
+  //post to messages to table
+  return next();
+};
+
+module.exports = {
+  getMessages,
+  addMessage,
+};
--- server/routes/messageRouter.js
+++ server/routes/messageRouter.js
@@ -1,1 +1,12 @@
-// use messagecontroller... for organization in routing
+const express = require("express");
+const db = require("../models/elephantsql");
+const router = express.Router();
+const { addMessage, getMessages } = require("../controllers/messageController");
+
+router.get("/", getMessages, (req, res) => {
+  return res.status(200).send(res.locals.messages);
+});
+
+router.post("/", addMessage, (req, res) => {
+  return res.status(200);
+});
