2704dcdb349ad1fbfb3b4883f91b9319e74e9dcc
jerikko <jerikkoagatep@gmail.com>
2022-09-01 17:45:20 -0700

    Completed workflow of consuming dead letter messages and displaying to front-end

--- rabbitmq/consume.js
+++ rabbitmq/consume.js
@@ -7,7 +7,7 @@ require("dotenv").config();
 const axios = require("axios");
 var amqp = require("amqplib/callback_api");
 
-amqp.connect(process.env.RABBIT_URI, function (error0, connection) {
+amqp.connect('amqps://xhvmtemw:wv7SvO0M_6pC28ICXh5JqrkmAKyj4-XJ@gull.rmq.cloudamqp.com/xhvmtemw', function (error0, connection) {
   if (error0) {
     throw error0;
   }
