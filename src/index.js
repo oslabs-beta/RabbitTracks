2dd98c0f7cceeb291ed342f58783f8c3502a5956
jerikko <jerikkoagatep@gmail.com>
2022-09-21 18:40:55 -0700

    Finished setting up Typescript config

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import App from "./App.jsx";
+import App from "./App.tsx";
 import styles from "./assets/stylesheets/global.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
