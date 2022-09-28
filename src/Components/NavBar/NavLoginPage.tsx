e3c20546268d220fad3b1cac02f605bdc2c3ad71
elrjolliffe <elrjolliffe@gmail.com>
2022-09-27 18:50:29 -0700

    Set up new routes and tsx component files

---
+++ src/Components/NavBar/NavLoginPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function NavLoginPage() {
+  return (
+    <div>NavLoginPage</div>
+  )
+}
+
+export default NavLoginPage
---
+++ src/Components/UserProjects.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+
+export default function UserProjects() {
+    
+    return (
+        <div>
+          
+        </div>
+    )
+}
