61a338b24e23b273c2a390bf96c7e3a2b0e9ef32
Jake Kazi <kazijake@gmail.com>
2022-10-29 16:26:45 -0400

    No change

--- src/__tests__/client.test.tsx
+++ src/__tests__/client.test.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import { render, fireEvent, screen, cleanup } from '@testing-library/react';
 import {describe, expect, test} from '@jest/globals';
-import "@testing-library/jest-dom/extend-expect"
-import { Route, Routes } from "react-router-dom";
+// import "@testing-library/jest-dom/extend-expect"
+import "@testing-library/jest-dom"
 import App from "../App";
 import Login from "../Components/Login";
 
@@ -21,16 +21,3 @@ test("does login button read the word login", async () => {
 //   const someElement = screen.getByTestId("");
 //   expect(breweriesElement).toBeInTheDocument();
 // });
-
-// describe("account delete form", () => {
-//   it("renders default state", () => {
-//     const { getByTestId } = render(<Login />);
-
-//     const password = getByTestId("account-delete-password") as HTMLInputElement;
-//     const confirm = getByTestId("account-delete-confirm");
-//     const submit = getByTestId("account-delete-submit");
-
-//     expect(password.value).toBe("");
-//     expect(confirm).not.toHaveClass("Mui-checked");
-//     expect(submit).toHaveClass("Mui-disabled");
-//   });
