fc450be6d58ddb2f63681302c550d315cafb7ffe
elrjolliffe <elrjolliffe@gmail.com>
2022-11-01 20:16:12 -0700

    Removed verifySession from get-all-messages route

--- server/routes/messageRouter.ts
+++ server/routes/messageRouter.ts
@@ -7,7 +7,6 @@ const messageController = require("../controllers/messageController");
 
 router.post(
   "/get-all-messages",
-  authController.verifySession,
   messageController.getAllMessages,
   (req: Request, res: Response) => {
     return res.status(200).json(res.locals.messages);
