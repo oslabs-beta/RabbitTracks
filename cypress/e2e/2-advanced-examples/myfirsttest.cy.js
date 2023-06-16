2ca34028339c6be8ba49b151d23b2aac10f84a38
Glen <glen.kasoff@gmail.com>
2023-06-15 23:49:27 -0400

    login page testing for cypress

--- cypress/e2e/2-advanced-examples/myfirsttest.cy.js
+++ cypress/e2e/2-advanced-examples/myfirsttest.cy.js
@@ -1,8 +1,8 @@
-context('First test', () => {
+context('Contain test', () => {
     beforeEach(() => {
         cy.visit("http://localhost:8080")
     })
     it('has an h1', () => {
-        cy.get('h1').should('exist')
+        cy.get('#welcome-statement').should('contain.text', 'Track. Reprocess.')
     })
 })
